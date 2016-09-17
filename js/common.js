cd5bee79c6f6158f5f5ceda5736ef780a34a70b9
Alex Miro <Alex Miro>
2016-09-17 15:45:35 +0300

    full prohect testing

--- js/common.js
+++ js/common.js
@@ -1,22 +1,12 @@
-
-
 $(document).ready(function (){
 
-	$("#scene").parallax({
-		// calibrateX: false,
-		// calibrateY: true,
-		// invertX: false,
-		// invertY: true,
-		// limitX: 50,
-		// limitY: 2,
-		// scalarX: 2,
-		// scalarY: 8,
-		// frictionX: 0.2,
-		// frictionY: 0.8,
-		// originX: 0.0,
-		// originY: 1.0
+	$("#scene").parallax();
+
+	$("#scroll-box").mCustomScrollbar({
+		theme:"rounded",
+		autoHideScrollbar: true,
+		mouseWheel:{ scrollAmount: 250 }
 	});
 
-	
-
 });
+
